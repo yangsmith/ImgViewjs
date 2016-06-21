10176187af4724fa68c260811e024c7b2213fd32
saya <1172069959@qq.com>
2016-06-21 18:07:33 +0800

    兼容性

--- js/showPic.js
+++ js/showPic.js
@@ -11,7 +11,25 @@ function showPic(whichpic) {
     description.firstChild.nodeValue = text;
 }
 
-function countBodyChildren(){
+function countBodyChildren() {
     var body_element = document.getElementsByTagName("body")[0];
     alert(body_element.childNodes.length);
+
+}
+
+window.onload = prepareLinks;
+function prepareLinks(){
+    var links = document.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++){
+        if (links[i].getAttribute("class") == "popup"){
+             links[i].onclick = function(){
+                 popUp(this.getAttribute("href"));
+                 return false;
+             }
+        }
+    }
+
+    function popUp(winURL) {
+        window.open(winURL,"popup","width=320,height=480");
+    }
 }
