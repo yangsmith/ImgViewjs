9cd71451979c5c6b98dc36a5b4c9aa45ccd35167
saya <1172069959@qq.com>
2016-06-22 17:43:47 +0800

    优化

--- js/showPic.js
+++ js/showPic.js
@@ -2,13 +2,19 @@
  * Created by saya on 2016/6/20.
  */
 function showPic(whichpic) {
+    if (!document.getElementById("placeholder"))  return false;
     var src = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src",src);
 
-    var text = whichpic.getAttribute("title");
+
     var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+    if (description){
+        var text = whichpic.getAttribute("title");
+        description.firstChild.nodeValue = text;
+    }
+
+    return true;
 }
 
 function countBodyChildren() {
@@ -19,6 +25,7 @@ function countBodyChildren() {
 
 window.onload = prepareLinks;
 function prepareLinks(){
+    if(!document.getElementsByTagName) return false;
     var links = document.getElementsByTagName("a");
     for(var i = 0; i < links.length; i++){
         if (links[i].getAttribute("class") == "popup"){
@@ -33,3 +40,36 @@ function prepareLinks(){
         window.open(winURL,"popup","width=320,height=480");
     }
 }
+
+function prepareGallery(){
+    if (!document.getElementsByName || !document.getElementById){
+        return false;
+    }
+
+    var gallery = document.getElementById("imagegallery");
+    if(!gallery){
+        return false;
+    }
+
+    var links = gallery.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++){
+        links[i].onclick = function() {
+            return !showPic(this);
+        }
+    }
+
+}
+
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function'){
+        window.onload = func;
+    }else {
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery)
